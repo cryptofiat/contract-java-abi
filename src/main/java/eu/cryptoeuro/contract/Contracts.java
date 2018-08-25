afde64360bab0defe383cf80a85231c97a75e721
Egon Elbre <egonelbre@gmail.com>
2018-08-25 15:29:23 +0300

    make runnable

--- src/main/java/eu/cryptoeuro/contract/Contracts.java
+++ src/main/java/eu/cryptoeuro/contract/Contracts.java
@@ -11,7 +11,7 @@ import java.math.BigInteger;
 public class Contracts {
     public static String RootAddress = "0xa10a263D4336E4466502b2889D27D04582a86663";
     public static BigInteger GasPrice = BigInteger.valueOf(1); //TODO:
-    public static BigInteger GasLimit = BigInteger.valueOf(1); //TODO:
+    public static BigInteger GasLimit = BigInteger.valueOf(400000); //TODO:
 
     public Web3j web3j;
     public Credentials credentials;
@@ -37,41 +37,37 @@ public class Contracts {
         return contracts;
     }
 
-    public static Contracts httpReadOnly() throws Exception {
-        return Contracts.loadReadonly(Web3j.build(new HttpService()));
-    }
-
     public static Contracts loadReadonly(Web3j web3j) throws Exception {
         Contracts contracts = new Contracts();
         contracts.web3j = web3j;
-        contracts.transactionManager = new ReadonlyTransactionManager(web3j, null);
+        contracts.transactionManager = new ReadonlyTransactionManager(web3j, RootAddress);
 
         contracts.initContractsReadOnly();
         return contracts;
     }
 
     private void initContracts() throws Exception {
-        CryptoFiat root = CryptoFiat.load(RootAddress, web3j, credentials, GasPrice, GasLimit);
+        this.root = CryptoFiat.load(RootAddress, web3j, credentials, GasPrice, GasLimit);
 
-        Data data = Data.load(addressOf(Index.Data), web3j, credentials, GasPrice, GasLimit);
-        Accounts accounts = Accounts.load(addressOf(Index.Accounts), web3j, credentials, GasPrice, GasLimit);
-        Approving approving = Approving.load(addressOf(Index.Approving), web3j, credentials, GasPrice, GasLimit);
-        Reserve reserve = Reserve.load(addressOf(Index.Reserve), web3j, credentials, GasPrice, GasLimit);
-        Enforcement enforcement = Enforcement.load(addressOf(Index.Enforcement), web3j, credentials, GasPrice, GasLimit);
-        AccountRecovery accountRecovery = AccountRecovery.load(addressOf(Index.AccountRecovery), web3j, credentials, GasPrice, GasLimit);
-        Delegation delegation = Delegation.load(addressOf(Index.Delegation), web3j, credentials, GasPrice, GasLimit);
+        this.data = Data.load(addressOf(Index.Data), web3j, credentials, GasPrice, GasLimit);
+        this.accounts = Accounts.load(addressOf(Index.Accounts), web3j, credentials, GasPrice, GasLimit);
+        this.approving = Approving.load(addressOf(Index.Approving), web3j, credentials, GasPrice, GasLimit);
+        this.reserve = Reserve.load(addressOf(Index.Reserve), web3j, credentials, GasPrice, GasLimit);
+        this.enforcement = Enforcement.load(addressOf(Index.Enforcement), web3j, credentials, GasPrice, GasLimit);
+        this.accountRecovery = AccountRecovery.load(addressOf(Index.AccountRecovery), web3j, credentials, GasPrice, GasLimit);
+        this.delegation = Delegation.load(addressOf(Index.Delegation), web3j, credentials, GasPrice, GasLimit);
     }
 
     private void initContractsReadOnly() throws Exception {
-        CryptoFiat root = CryptoFiat.load(RootAddress, web3j, transactionManager, GasPrice, GasLimit);
+        this.root = CryptoFiat.load(RootAddress, web3j, transactionManager, GasPrice, GasLimit);
 
-        Data data = Data.load(addressOf(Index.Data), web3j, transactionManager, GasPrice, GasLimit);
-        Accounts accounts = Accounts.load(addressOf(Index.Accounts), web3j, transactionManager, GasPrice, GasLimit);
-        Approving approving = Approving.load(addressOf(Index.Approving), web3j, transactionManager, GasPrice, GasLimit);
-        Reserve reserve = Reserve.load(addressOf(Index.Reserve), web3j, transactionManager, GasPrice, GasLimit);
-        Enforcement enforcement = Enforcement.load(addressOf(Index.Enforcement), web3j, transactionManager, GasPrice, GasLimit);
-        AccountRecovery accountRecovery = AccountRecovery.load(addressOf(Index.AccountRecovery), web3j, transactionManager, GasPrice, GasLimit);
-        Delegation delegation = Delegation.load(addressOf(Index.Delegation), web3j, transactionManager, GasPrice, GasLimit);
+        this.data = Data.load(addressOf(Index.Data), web3j, transactionManager, GasPrice, GasLimit);
+        this.accounts = Accounts.load(addressOf(Index.Accounts), web3j, transactionManager, GasPrice, GasLimit);
+        this.approving = Approving.load(addressOf(Index.Approving), web3j, transactionManager, GasPrice, GasLimit);
+        this.reserve = Reserve.load(addressOf(Index.Reserve), web3j, transactionManager, GasPrice, GasLimit);
+        this.enforcement = Enforcement.load(addressOf(Index.Enforcement), web3j, transactionManager, GasPrice, GasLimit);
+        this.accountRecovery = AccountRecovery.load(addressOf(Index.AccountRecovery), web3j, transactionManager, GasPrice, GasLimit);
+        this.delegation = Delegation.load(addressOf(Index.Delegation), web3j, transactionManager, GasPrice, GasLimit);
     }
 
     private String addressOf(int v) throws Exception {
--- src/main/java/eu/cryptoeuro/example/ReadOnlyExample.java
+++ src/main/java/eu/cryptoeuro/example/ReadOnlyExample.java
@@ -11,18 +11,35 @@ import org.web3j.protocol.http.*;
 import org.web3j.protocol.core.*;
 import org.web3j.protocol.core.methods.request.*;
 import org.web3j.protocol.core.methods.response.*;
+import org.web3j.protocol.ipc.*;
 import org.web3j.tx.*;
 
 import java.math.BigInteger;
 
 public class ReadOnlyExample {
-    public static void main() throws Exception {
-        Contracts contracts = Contracts.httpReadOnly();
 
-        String reserveBank = contracts.reserve.reserveBank().send();
-        System.out.println(reserveBank);
+    public static void main(String[] args) throws Exception {
+        Web3jService service;
+        // service = new HttpService("https://mainnet.infura.io/");
+        service = new HttpService();
+        Web3j web3j = Web3j.build(service);
+        try {
+            System.out.println(web3j.web3ClientVersion().send().getWeb3ClientVersion());
 
-        // TransactionReceipt txr = contracts.accounts.transfer("", BigInteger.valueOf(1500)).send();
+            TransactionManager transactionManager = new ReadonlyTransactionManager(web3j, null);
+            CryptoFiat root = CryptoFiat.load("0xa10a263D4336E4466502b2889D27D04582a86663", web3j, transactionManager, BigInteger.valueOf(1), BigInteger.valueOf(1));
+            BigInteger contractsLength = root.contractsLength().send();
+
+            System.out.println(contractsLength);
+
+            //Contracts contracts = Contracts.loadReadonly(web3j);
+
+            //String reserveBank = contracts.reserve.reserveBank().send();
+            //System.out.println(reserveBank);
+
+            // TransactionReceipt txr = contracts.accounts.transfer("", BigInteger.valueOf(1500)).send();
+        } finally {
+            web3j.shutdown();
+        }
     }
-
 }
